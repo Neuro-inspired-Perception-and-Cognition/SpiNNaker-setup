d4c0bbe63e9d372690d5cbe192b8740170fbcb05
Vedmedenko Olha <vedmeolh@fit.cvut.cz>
2026-04-30 16:39:43 +0200

    Update example.py

--- example.py
+++ example.py
@@ -4,6 +4,3 @@ p.setup(timestep=1.0)
 pop = p.Population(3, p.IF_curr_exp(), label="MyPopulation")
 p.run(100)
 p.end()
-
-
-# netsh interface ipv4 set address name="Ethernet" static 130.88.198.99 255.255.255.0 0.0.0.0
